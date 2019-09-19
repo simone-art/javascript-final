337576a0423a5abb7e03e475136f2d7978591028
simone-art <simoneperiodista3@gmail.com>
2019-09-19 17:23:05 -0300

    finalizei exercicios java

---
+++ i.js
@@ -0,0 +1,14 @@
+function positivoNegativo(a,b){
+    if (a>=0 && b>=0) {
+        console.log("positivo")
+    } else
+        console.log("negativo")
+    }
+
+    function positivoNegativo(a,b){
+        if (a>=0 && b>=0) {
+            console.log("positivo")
+        } else if (a<0 && b<0) 
+            {console.log("negativo")}
+        }
+        
